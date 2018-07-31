8ac6912238b25462721c7ef1c37a08d7510fc390
jeroenransijn <jssrdesign@gmail.com>
2018-07-30 17:25:27 -0700

    improve table story

--- src/table/stories/VirtualTable.js
+++ src/table/stories/VirtualTable.js
@@ -2,6 +2,8 @@ import React from 'react'
 import faker from 'faker'
 import { Table } from '../'
 import { Pane } from '../../layers'
+import { SegmentedControl } from '../../segmented-control'
+import { Checkbox } from '../../checkbox'
 import { Paragraph } from '../../typography'
 
 const range = N => Array.from({ length: N }, (v, k) => k + 1)
@@ -12,54 +14,132 @@ const randomLengthContent = [
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
 ]
 
-// Generate a bunch of users.
-const users = range(1000)
-  .map(() => ({
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    height: faker.random.arrayElement([32, 40, 56, 'auto'])
-  }))
-  .map(item => {
-    // When height is auto, use a variable length piece of content to render.
-    if (item.height === 'auto') {
-      return {
-        ...item,
-        content: faker.random.arrayElement(randomLengthContent)
+const createUsers = (amount, allowAutoHeight) => {
+  return range(amount)
+    .map(() => ({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      height: faker.random.arrayElement([
+        32,
+        40,
+        56,
+        allowAutoHeight ? 'auto' : 48
+      ])
+    }))
+    .map(item => {
+      // When height is auto, use a variable length piece of content to render.
+      if (item.height === 'auto') {
+        return {
+          ...item,
+          content: faker.random.arrayElement(randomLengthContent)
+        }
       }
-    }
-    return item
-  })
+      return item
+    })
+}
 
 export default class VirtualTable extends React.PureComponent {
+  state = {
+    totalUsers: 500,
+    users: createUsers(500, true),
+    allowAutoHeight: true
+  }
+
+  handleChange = value => {
+    const totalUsers = Number(value)
+
+    this.setState({
+      totalUsers,
+      users: createUsers(totalUsers, this.state.allowAutoHeight)
+    })
+
+    this.forceUpdate()
+  }
+
+  handleCheckboxChange = e => {
+    this.setState({
+      allowAutoHeight: e.target.checked,
+      users: createUsers(this.state.totalUsers, e.target.checked)
+    })
+
+    this.forceUpdate()
+  }
+
   render() {
     return (
-      <Pane border height="80vh" display="flex" flexGrow={0}>
-        <Table flex={1} display="flex" flexDirection="column">
-          <Table.Head>
-            <Table.TextHeaderCell>Index</Table.TextHeaderCell>
-            <Table.TextHeaderCell>Height</Table.TextHeaderCell>
-            <Table.TextHeaderCell>Name</Table.TextHeaderCell>
-            <Table.TextHeaderCell>Email</Table.TextHeaderCell>
-          </Table.Head>
-          <Table.VirtualBody flex={1} allowAutoHeight>
-            {users.map((user, index) => {
-              return (
-                <Table.Row key={user.email} height={user.height}>
-                  <Table.TextCell>{index}</Table.TextCell>
-                  <Table.TextCell>{user.height}</Table.TextCell>
-                  <Table.TextCell>{user.name}</Table.TextCell>
-                  {user.height === 'auto' ? (
-                    <Table.Cell>
-                      <Paragraph marginY={24}>{user.content}</Paragraph>
-                    </Table.Cell>
-                  ) : (
-                    <Table.TextCell>This is a static height row</Table.TextCell>
-                  )}
-                </Table.Row>
-              )
-            })}
-          </Table.VirtualBody>
-        </Table>
+      <Pane>
+        <Pane>
+          <SegmentedControl
+            options={[
+              {
+                label: '500',
+                value: 500
+              },
+              {
+                label: '1.000',
+                value: 1000
+              },
+              {
+                label: '10.000',
+                value: 10000
+              },
+              {
+                label: '50.000',
+                value: 50000
+              },
+              {
+                label: '100.000',
+                value: 100000
+              }
+            ]}
+            onChange={this.handleChange}
+            value={this.state.totalUsers}
+          />
+          <Checkbox
+            checked={this.state.allowAutoHeight}
+            onChange={this.handleCheckboxChange}
+            label="Allow auto heights"
+          />
+        </Pane>
+
+        <Pane border height="80vh" display="flex" flexGrow={0}>
+          <Table
+            key={this.state.totalUsers}
+            flex={1}
+            display="flex"
+            flexDirection="column"
+          >
+            <Table.Head>
+              <Table.TextHeaderCell>Index</Table.TextHeaderCell>
+              <Table.TextHeaderCell>Height</Table.TextHeaderCell>
+              <Table.TextHeaderCell>Name</Table.TextHeaderCell>
+              <Table.TextHeaderCell>Email</Table.TextHeaderCell>
+            </Table.Head>
+            <Table.VirtualBody
+              flex={1}
+              allowAutoHeight={this.state.allowAutoHeight}
+            >
+              {this.state.users.map((user, index) => {
+                return (
+                  <Table.Row key={user.email} height={user.height}>
+                    <Table.TextCell>{index}</Table.TextCell>
+                    <Table.TextCell>{user.height}</Table.TextCell>
+                    <Table.TextCell>{user.name}</Table.TextCell>
+                    {user.height === 'auto' ? (
+                      <Table.Cell>
+                        <Paragraph marginY={24}>{user.content}</Paragraph>
+                      </Table.Cell>
+                    ) : (
+                      <Table.TextCell>
+                        This is a static height row
+                      </Table.TextCell>
+                    )}
+                  </Table.Row>
+                )
+              })}
+            </Table.VirtualBody>
+          </Table>
+        </Pane>
       </Pane>
     )
   }
